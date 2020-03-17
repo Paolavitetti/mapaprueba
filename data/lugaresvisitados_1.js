64f2729a1830cace247178b14fb1e5be0a5a7bb1
paolavitetti <62195151+Paolavitetti@users.noreply.github.com>
2020-03-17 01:36:11 -0300

    primera edicion

---
+++ data/lugaresvisitados_1.js
@@ -0,0 +1,9 @@
+var json_lugaresvisitados_1 = {
+"type": "FeatureCollection",
+"name": "lugaresvisitados_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nombre": "Bogotá", "interesant": "depende de la hora es mejor viajar en transmilenio" }, "geometry": { "type": "Point", "coordinates": [ -74.090228566449838, 4.713909463058197 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Nombre": "Medellin", "interesant": "historia por todos lados, muy organizado" }, "geometry": { "type": "Point", "coordinates": [ -75.561301986524185, 6.216500752770925 ] } }
+]
+}
---
+++ data/rutas_2.js
@@ -0,0 +1,8 @@
+var json_rutas_2 = {
+"type": "FeatureCollection",
+"name": "rutas_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "nombre": "Bogota\/Medellin", "transporte": "Colectivo" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -74.103265131273247, 4.713909463058194 ], [ -74.333806488150572, 4.905350681283648 ], [ -74.77293288220261, 5.326325191628177 ], [ -74.934860740009299, 5.594069442653215 ], [ -75.401432533689601, 6.060959061506016 ], [ -75.54689315171936, 6.194673087834933 ] ] ] } }
+]
+}
